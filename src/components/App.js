53b22d5069abcedaa91fa30f35eccf98c102cb1a
lukimann <lukewanyama5@gmail.com>
2024-02-10 17:57:08 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route  } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
-function App() {
-  return <div>{/*{code here}*/}</div>;
+    function App() {
+      return <>
+         <NavBar />
+      <div >
+        <Route path="/" component={Home}/>
+        <Route path="/movies" component={Movies}/>
+        <Route path="/actors" component={Actors}/>
+        <Route path="/directors" component={Directors}/>
+      </div>
+    
+    </>;
 }
 
 export default App;
+
